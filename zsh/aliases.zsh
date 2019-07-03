f0e88fefe3aab70af97a44d0ac4b808c97836a8c
Hatfim Ydniw <hatfim.ydniw@gmail.com>
2019-07-03 11:09:01 +0700

    add zplug

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,31 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Enable tab completion of flags
+source $(dirname $(gem which colorls))/tab_complete.sh
+
+# Move standard ls
+alias ols="ls"
+# Base formats
+
+alias ls="colorls --group-directories-first --almost-all"          # short, multi-line
+alias ll="colorls --group-directories-first --almost-all --long"   # list, 1 per line
+alias ld="ll"                   # ^^^, NOTE: Trying to move to this for alternate hand commands
+alias la="colorls -lA"          # list w/ info
+# [d] Sort output with directories first
+alias lsd="ls --sort-dirs"
+alias lld="ll --sort-dirs"
+alias ldd="ld --sort-dirs"
+alias lad="la --sort-dirs"
+
+# [t] Sort output with recent modified first
+alias lst="ls -t"
+alias llt="ll -t"
+alias ldt="ld -t"
+alias lat="la -t"
+# [g] Add git status of each item in output
+alias lsg="ls --git-status"
+alias llg="ll --git-status"
+alias ldg="ld --git-status"
+alias lag="la --git-status"
